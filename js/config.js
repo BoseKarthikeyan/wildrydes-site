dc716e9de5e7f091eedbec26e974939070cc47c1
BoseKarthikeyan <malkarthi@gmail.com>
2020-07-19 19:35:53 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_U8tbldBJW', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '58dj56tboubhb3nehi4ib185h1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_8NEpvkFE1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6uae0nkqficb02q8e38tamfnla', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
